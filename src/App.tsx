e63529479b15ec1ba97b168cc48bfc54b834321e
Siddhesh <siddhehsongire@gmail.com>
2023-01-02 21:52:11 +0530

    Setting up routes thorugh react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+import { Navbar } from './components/navbar';
+import { Login } from './pages/login';
+import { Main } from './pages/main';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello 😎</h1>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element = {<Main/>}/>
+          <Route path="/login" element = {<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,3 @@
+export const Login = () => {
+    return <h1> This is LOGIN page </h1>
+}
