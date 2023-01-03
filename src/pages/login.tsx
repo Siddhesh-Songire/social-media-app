5643e84814d62e9901047d3eb1b7e2fdeb7b6fd0
Siddhesh <siddhehsongire@gmail.com>
2023-01-03 19:38:19 +0530

    sign in with google using firebase

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,15 @@
+import { auth, provider} from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+
+
 export const Login = () => {
-    return <h1> This is LOGIN page </h1>
-}
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider); 
+    }
+    return(
+    <div>
+        <p> <h1>Sign in with Google to continue...</h1></p>
+        <button onClick={signInWithGoogle}> SignIn</button>
+    </div>
+    );
+};
