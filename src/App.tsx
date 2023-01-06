8340f2ad8edcc814fd7f42c9a8d3fd83081c7810
Siddhesh <siddhehsongire@gmail.com>
2023-01-06 16:48:48 +0530

    creating createpost page and route

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 import { Navbar } from './components/navbar';
+import { CreatePost } from './pages/createpost';
 import { Login } from './pages/login';
 import { Main } from './pages/main';
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element = {<Main/>}/>
           <Route path="/login" element = {<Login/>}/>
+          <Route path="/createpost" element = {<CreatePost/>}/>
         </Routes>
       </Router>
     </div>
---
+++ src/pages/createpost.tsx
@@ -0,0 +1,5 @@
+export const CreatePost = () => {
+    return(
+    <h1> This is CreatePost page </h1>
+    );
+};
