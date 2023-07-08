2bb6099890c41bda1ff9666dea00f06fa41e1a5f
Siddhesh <siddhehsongire@gmail.com>
2023-07-08 22:49:58 +0530

    made some css changes

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,45 +1,44 @@
-import { getDocs, collection, doc } from "firebase/firestore";
+import { getDocs, collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { db } from "../../config/firebase"
+import { db } from "../../config/firebase";
 import { Post } from "./post";
 import { auth } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Landing } from "./landing";
 
 export interface Post {
-    id: string;
-    userId : string;
-    title : string;
-    username : string;
-    description : string;
+  id: string;
+  userId: string;
+  title: string;
+  username: string;
+  description: string;
 }
 
 export const Main = () => {
-    const[postList, setPostList] = useState<Post[] | null>(null);
-    const postsRef = collection(db, "posts");
+  const [postList, setPostList] = useState<Post[] | null>(null);
+  const postsRef = collection(db, "posts");
 
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const getPosts = async () => {
-        const data = await getDocs(postsRef);
-        setPostList(
-            data.docs.map((doc) => ({...doc.data(), id: doc.id })) as Post[]
-        );
-
-    };
-
-    useEffect(() => {
-        getPosts();
-    }, []);
-
-    return(
-        <div>
-            <h1> This is the HOME page </h1>
-            {!user && <Landing/> }
-            {postList?.map((post) => (
-                <Post post = {post} />
-            ))}
-        </div>
-     
+  const getPosts = async () => {
+    const data = await getDocs(postsRef);
+    setPostList(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
     );
-}
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <div className="container my-4 d-flex flex-wrap">
+        {!user && <Landing />}
+        {postList?.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
+};
